bbe80c45c54d3a70408de9a2fab59df5228626b1
lplk <effy1990@yahoo.com>
2020-06-29 20:38:28 +0800

    reuse CRUD middleware

--- admin/src/views/CategoryList.vue
+++ admin/src/views/CategoryList.vue
@@ -25,7 +25,7 @@ export default {
   },
   methods: {
     async fetch() {
-      const res = await this.$http.get("categories");
+      const res = await this.$http.get("rest/categories");
       this.items = res.data;
       console.log(res.data);
     },
@@ -35,7 +35,7 @@ export default {
           cancelButtonText: '取消',
           type: 'warning'
         }).then(async() => {
-            await this.$http.delete(`categories/${row._id}`)
+            await this.$http.delete(`rest/categories/${row._id}`)
 
           this.$message({
             type: 'success',
--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -1,28 +1,42 @@
 module.exports = app => {
     const express = require('express')
-    const router = express.Router()
-    const Category = require('../../models/Category')
-    router.post('/categories', async(req, res) => {
-        const model = await Category.create(req.body)
+    const router = express.Router({
+        mergeParams: true
+    })
+    //const Category = require('../../models/Category')
+    router.post('/', async(req, res) => {
+        const model = await req.Model.create(req.body)
         res.send(model)
     })
-    router.put('/categories/:id', async(req, res) => {
-        const model = await Category.findByIdAndUpdate(req.params.id, req.body)
+    router.put('/:id', async(req, res) => {
+        const model = await req.Model.findByIdAndUpdate(req.params.id, req.body)
         res.send(model)
     })
-    router.get('/categories', async(req, res) => {
-        const items = await Category.find().populate('parent').limit(10)
+    router.get('/', async(req, res) => {
+        const queryOptions = {}
+        
+        if(req.Model.modelName === 'Category'){
+            queryOptions.populate = 'parent'
+        }
+        
+        const items = await req.Model.find().setOptions(queryOptions).limit(10)
         res.send(items)
     })
-    router.get('/categories/:id', async(req, res) => {
-        const model = await Category.findById(req.params.id)
+    router.get('/:id', async(req, res) => {
+        const model = await req.Model.findById(req.params.id)
         res.send(model)
     })
-    router.delete('/categories/:id', async(req, res) => {
-        const model = await Category.findByIdAndDelete(req.params.id)
+    router.delete('/:id', async(req, res) => {
+        const model = await req.Model.findByIdAndDelete(req.params.id)
         res.send({
             seccess: true
         })
     })
-    app.use('/admin/api', router)
+    app.use('/admin/api/rest/:resource',(req, res, next) => {
+        //小写复数转大写单数
+        const modelName = require('inflection').classify(req.params.resource)
+        req.Model = require(`../../models/${modelName}`)
+
+        next()
+    } , router)
 }
