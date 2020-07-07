4a108fbd687d33ce6bdddda11f5fd43459c390bb
lplk <effy1990@yahoo.com>
2020-07-07 23:12:39 +0800

    hero edit & list

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -4,8 +4,10 @@ import VueRouter from 'vue-router'
 import Main from '../views/Main.vue'
 import CategoryEdit from '../views/CategoryEdit.vue'
 import ItemEdit from '../views/ItemEdit.vue'
+import HeroEdit from '../views/HeroEdit.vue'
 import CategoryList from '../views/CategoryList.vue'
 import ItemList from '../views/ItemList.vue'
+import HeroList from '../views/HeroList.vue'
 Vue.use(VueRouter)
 
   const routes = [
@@ -21,6 +23,10 @@ Vue.use(VueRouter)
       { path: 'items/create' , component: ItemEdit},
       { path: 'items/list' , component: ItemList},
       { path: 'items/edit/:id' , component: ItemEdit, props: true},
+    
+      { path: 'heroes/create' , component: HeroEdit},
+      { path: 'heroes/list' , component: HeroList},
+      { path: 'heroes/edit/:id' , component: HeroEdit, props: true},
     
     ]
   },
